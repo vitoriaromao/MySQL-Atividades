40f0287957ae05bee65c7eb3b4cca84a2f44dbf8
vitoria <vitoria.rmoura2@hotmail.com>
2022-01-24 22:03:24 -0300

    Atividade de Spring

---
+++ Spring-boot/minhalojadegames/src/main/java/br/org/generation/minhalojadegames/model/Categoria.java
@@ -0,0 +1,49 @@
+package br.org.generation.minhalojadegames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id ;
+	
+	@NotBlank(message = "O atributo tipo é obrigatório!")
+	@Size (min = 5, max = 100, message = "O atributo título deve conter no mínimo 5 caractéres e no máximo 100 caractéres!")
+	private String tipo ;
+	
+	@OneToMany (mappedBy = "categoria", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+}
---
+++ Spring-boot/minhalojadegames/src/main/java/br/org/generation/minhalojadegames/model/Produto.java
@@ -0,0 +1,115 @@
+package br.org.generation.minhalojadegames.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id ;
+	
+	@NotNull (message = "O nome é obrigatório!")
+	@Size (max = 100)
+	private String nome;
+	
+	@Size (max = 300)
+	private String descricao;
+	
+	@NotNull (message = "O Console é obrigatório!")
+	@Size (max = 50)
+	private String console;
+	
+	@NotNull (message = "A quantidade é obrigatório!")
+	private Long quantidade; 
+	
+	@Column(name = "data_lancamento")
+	@JsonFormat(pattern = "yyyy-MM-dd")
+    private LocalDate dataLancamento;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+	@NotNull (message ="Preço é obrigatório!")
+	private BigDecimal preco ;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	public Long getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(Long quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public LocalDate getDataLancamento() {
+		return dataLancamento;
+	}
+
+	public void setDataLancamento(LocalDate dataLancamento) {
+		this.dataLancamento = dataLancamento;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria_id() {
+		return categoria;
+	}
+
+	public void setCategoria_id(Categoria categoria_id) {
+		this.categoria = categoria;
+	}
+}
