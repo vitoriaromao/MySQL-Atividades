0102816f25910b347079c9f3ca440c7db3360812
vitoria <vitoria.rmoura2@hotmail.com>
2022-02-01 16:26:27 -0300

    Adicionando os testes no blog pessoal

--- Spring-boot/blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/UsuarioRepository.java
+++ Spring-boot/blogpessoal/src/main/java/br/org/generation/blogpessoal/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package br.org.generation.blogpessoal.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,11 +11,8 @@ import br.org.generation.blogpessoal.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-	/**
-	 * Método que busca um usuário pelo seu usuario (email).
-	 * 
-	 * select * from tb_usuarios where usuario = "usuario procurado"
-	 */
 	public Optional<Usuario> findByUsuario(String usuario);
+	
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
 	
 }
